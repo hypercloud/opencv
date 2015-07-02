e8e93ec6403a6f5b96050e60f77075e188e765d2
kevinprotoss <kevinprotoss.wei@gmail.com>
2015-07-02 10:26:37 +0200

    update Opencv 3.0 version

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,11 @@
 FROM node:0.12.5-slim
+MAINTAINER Junxiang Wei <junxiang.wei@nodeunify.com>
 
 RUN apt-get update \
 	&& DEBIAN_FRONTEND=noninteractive \
-	apt-get install -y libopencv-dev
+	apt-get install -y curl
 
-# Known issue:
-# libdc1394 error: Failed to initialize libdc1394
-# Solution: disable driver
-# Ref: http://stackoverflow.com/questions/12689304/ctypes-error-libdc1394-error-failed-to-initialize-libdc1394
-# Do the following in the container later for ignoring the warning
-# sudo ln /dev/null /dev/raw1394
-# RUN command in Dockerfile doesn't work, since docker creates and copys volume of default filesystem each time
+# Add scripts to install opencv
+ADD ./scripts /scripts
 
-WORKDIR /usr/src/app
-
-CMD ["npm", "start"]
+RUN ./scripts/install.sh
---
+++ scripts/install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "Installing OpenCV"
+
+# Compiler
+apt-get install -y build-essential
+# Required packages
+apt-get install -y cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+# Optional packages
+apt-get install -y python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
+
+# Download
+curl -L https://github.com/Itseez/opencv/archive/3.0.0.tar.gz | tar xz
+
+# Install
+cd opencv-3.0.0
+mkdir build
+cd build
+cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local ..
+make -j2
+make install
+
+# Known issue:
+# libdc1394 error: Failed to initialize libdc1394
+# Solution: disable driver
+# Ref: http://stackoverflow.com/questions/12689304/ctypes-error-libdc1394-error-failed-to-initialize-libdc1394
+# RUN command in Dockerfile doesn't work, since docker creates and copys volume of default filesystem each time
+ln /dev/null /dev/raw1394
+
+echo "OpenCV ready to be used"
+
